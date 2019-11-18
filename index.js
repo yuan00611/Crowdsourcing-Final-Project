2dc5d0038ebdd178a27e11376fecd4b18b5872cf
陳德元 <r06521608@ntu.edu.tw>
2019-11-18 17:07:48 -0600

    add function undo&clear all

--- index.js
+++ index.js
@@ -1,49 +1,101 @@
-var i = 0
+var i = 0;
+var text = [];
 
 function labelText()
 {
 	var selection = window.getSelection();
 	var range = selection.getRangeAt(0);
-	
+  //console.log(range);
+  console.log(range.startContainer.parentElement.nodeName);
+  
 
-	if (range.collapsed == false) {
+  //var c = String(range.startContainer);
+  //console.log(c[1]);
+
+	if (range.collapsed == false && range.startOffset != 1 && i < 5 && range.startContainer.parentElement.nodeName == "P") {
 		var node = document.createElement("span");
 		node.style.backgroundColor = 'yellow';
+    node.setAttribute("id", i);
+    //console.log(node);
 		i += 1;
 		range.surroundContents(node);
-		console.log(node.innerText);
+		//console.log(node.innerText);
 		//console.log(range.commonAncestorContainer.outerText);
 
+    l = document.getElementsByClassName("rankList");
+
+    addOption(l, node.innerText);
+    text.push(node.innerText);
+    //console.log(text);
+    //console.log(l.length);
+
+/* drag div
 		var d = document.createElement("div");
     d.setAttribute("class", "drag")
 		d.innerText = node.innerText;
 		d.style.position = "absolute";
 		dragElement(d);
 		document.getElementById("dragArea").appendChild(d);
-
+*/
 	}
-
 	
 }
 
 function undo(){
-	var selection = window.getSelection();
-	var range = selection.getRangeAt(0);
+  if(i >= 0){
+    i = i - 1;
+    last = document.getElementById(i);
+    last.insertAdjacentText("afterend", text[text.length - 1]);
+  
+    text.pop();
+    last.parentNode.removeChild(last);
 
-	
-	
-	//console.log(range);
-	//last = document.getElementById(i-1);
-	//last.style.backgroundColor = 'white';
-	//i -= 2;
+    l = document.getElementsByClassName("rankList");
+    deleteOption(l);
+  }
 }
 
-function clear(){
-	
+function clearAll(){
+  for(var j = i - 1; j >= 0; j--){
+
+    last = document.getElementById(j);
+    last.insertAdjacentText("afterend", text[text.length - 1]);
+  
+    text.pop();
+    last.parentNode.removeChild(last);
+
+    l = document.getElementsByClassName("rankList");
+    deleteOption(l);
+  }
+  i = 0;
+}
+
+function addOption(list, text){
+  for(var i = 0; i < list.length; i++){
+    var index = list[i].options.length;
+    list[i].options[index] = new Option(text, text);
+  }
+}
+
+function deleteOption(list){
+  for(var i = 0; i < list.length; i++){
+    var index = list[i].options.length - 1;
+    list[i].options[index] = null;
+  }
+  //var index = list.selectedIndex;
+  //list.options[index] = null;
 }
 
 
 
+
+
+
+
+
+
+
+/*
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
@@ -84,3 +136,4 @@ function dragElement(elmnt) {
     document.onmousemove = null;
   }
 }
+*/
